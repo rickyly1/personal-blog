fc223ac4829fced2f58c67a4bbba96701ef629d6
rickyly1 <lyricky123@gmail.com>
2023-11-03 14:58:55 -0700

    encountered web_accessible_resources manifest key error

--- server/routes/main.js
+++ server/routes/main.js
@@ -35,9 +35,61 @@ router.get('', async (req, res) => {
 
 });
 
+/**
+ * GET /
+ * Post: id
+ */
+router.get('/post/:id', async (req, res) => {
+    try {
+        let slug = req.params.id;
+
+        const data = await Post.findById({_id: slug});
+
+        const locals = {
+            title: data.title,
+            description: data.body
+        }
+
+        res.render('post', {locals, data});
+
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+/**
+ * POST /
+ * Post : searchTerm
+ */
+router.post('/search', async (req, res) => {
+    try {
+        const locals = {
+            title: "Search",
+            description: "Simple Blog created with NodeJs, Express, & MongoDB."
+        }
+
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g, "");
+
+        const data = await Post.find({
+            $or: [
+                {title: {$regex: new RegExp(searchNoSpecialChar, 'i')}},
+                {body: {$regex: new RegExp(searchNoSpecialChar, 'i')}}
+            ]
+        })
+
+        res.render("search", {
+            data,
+            locals
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 router.get('/about', (req, res) => {
     res.render('about');
-})
+});
 
 // function insertPostData() {
 //     Post.insertMany([
